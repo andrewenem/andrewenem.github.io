d312f8837171c4f152d804551c6e80dfb7ca34d5
andrewenem <andrewenem@gmail.com>
2023-02-11 12:45:09 -0300

    Ícones de redes sociais, botão de menu mais interativo e alinhamento do card de média.

--- js.js
+++ js.js
@@ -2,9 +2,13 @@ window.addEventListener('load', () => {
 
     document.getElementById("menu-button").addEventListener('click', () => {
         if (document.getElementById("nav-modal").classList.contains("show")) {
-            document.getElementById("nav-modal").classList.remove("show")
+            document.getElementById("nav-modal").classList.remove("show");
+            document.getElementById("menu-button").getElementsByTagName("i")[0].classList.add("fa-bars");
+            document.getElementById("menu-button").getElementsByTagName("i")[0].classList.remove("fa-xmark");
         } else {
             document.getElementById("nav-modal").classList.add("show")
+            document.getElementById("menu-button").getElementsByTagName("i")[0].classList.remove("fa-bars");
+            document.getElementById("menu-button").getElementsByTagName("i")[0].classList.add("fa-xmark");
         }
     })
 
