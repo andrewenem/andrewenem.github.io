9f73b80301b159cc0f70e5260dc36d0188bd5945
andrewenem <andrewenem@gmail.com>
2024-02-06 12:04:16 -0300

    Informações Mentoria

--- js.js
+++ js.js
@@ -3,12 +3,32 @@ window.addEventListener('load', () => {
     document.getElementById("menu-button").addEventListener('click', () => {
         if (document.getElementById("nav-modal").classList.contains("show")) {
             document.getElementById("nav-modal").classList.remove("show");
+			document.getElementById("modal-container").classList.remove("show");
             document.getElementById("menu-button").getElementsByTagName("i")[0].classList.add("fa-bars");
             document.getElementById("menu-button").getElementsByTagName("i")[0].classList.remove("fa-xmark");
         } else {
+			document.getElementById("modal-container").classList.add("show");
             document.getElementById("nav-modal").classList.add("show")
             document.getElementById("menu-button").getElementsByTagName("i")[0].classList.remove("fa-bars");
             document.getElementById("menu-button").getElementsByTagName("i")[0].classList.add("fa-xmark");
+        }
+    })
+	
+	document.getElementById("modal-container").addEventListener('click', () => {
+		document.getElementById("nav-modal").classList.remove("show");
+		document.getElementById("benefits-modal").classList.remove("show");
+		document.getElementById("modal-container").classList.remove("show");
+		document.getElementById("menu-button").getElementsByTagName("i")[0].classList.add("fa-bars");
+		document.getElementById("menu-button").getElementsByTagName("i")[0].classList.remove("fa-xmark");
+	})
+	
+	document.getElementById("show-mentoria-benefits").addEventListener('click', () => {
+        if (document.getElementById("benefits-modal").classList.contains("show")) {
+            document.getElementById("benefits-modal").classList.remove("show");
+			document.getElementById("modal-container").classList.remove("show");
+        } else {
+            document.getElementById("benefits-modal").classList.add("show")
+			document.getElementById("modal-container").classList.add("show");
         }
     })
 	
